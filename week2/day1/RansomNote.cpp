07991216b4317ea04950cf94aed58002b6ed550e
aarav-sri-kali <aaravkayasth@gmail.com>
2026-06-08 23:22:40 +0530

    Update RansomNote.cpp

--- week2/day1/RansomNote.cpp
+++ week2/day1/RansomNote.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int hash[26]={0};
+        if(magazine.size()<ransomNote.size()){
+            return false;
+        }
+        for(int j=0;j<magazine.size();j++){
+            hash[magazine[j]-97]++; 
+        }
+        for(int j=0;j<ransomNote.size();j++){
+            hash[ransomNote[j]-97]--; 
+           if(hash[ransomNote[j]-97]<0){
+            return false;
+           }                                   
+        }
+        return true;
+    }
+};
